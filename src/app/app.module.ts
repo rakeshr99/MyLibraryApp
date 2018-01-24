cfca4ee37835a157099a0cb82c93f2775043e5ab
Radhakrishnan <rakesh.radhakrishnan@philips.com>
2018-01-24 10:23:57 -0500

    end of lab 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { CollectionComponent } from './collection/collection.component';
+import { RatingCategoryPipe } from './pipes/rating-category.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    CollectionComponent
+    CollectionComponent,
+    RatingCategoryPipe
   ],
   imports: [
     BrowserModule,
