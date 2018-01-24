7b0fbd27749a9f3250cd6b0c9636c83aa5001247
Radhakrishnan <rakesh.radhakrishnan@philips.com>
2018-01-24 11:57:05 -0500

    end of lab 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { DataService } from '../app/services/data.service';
 //comment
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DataService]
 })
 export class AppComponent {
   title = 'My tiny library App!!';
