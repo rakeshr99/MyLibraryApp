5608ee28da29d853db70f8e79b6b5bc9dac85801
Radhakrishnan <rakesh.radhakrishnan@philips.com>
2018-01-24 15:29:15 -0500

    end of lab 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutComponent } from './about/about.component';
 import { CollectionComponent } from './collection/collection.component';
 import { RatingCategoryPipe } from './pipes/rating-category.pipe';
 import { RatingComponent } from './rating/rating.component';
+import { HttpModule } from '@angular/http';
 
 
 @NgModule({
@@ -38,7 +39,8 @@ import { RatingComponent } from './rating/rating.component';
     MatLineModule,
     MatInputModule,
     MatToolbarModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Ibook } from '../ibook';
 import { MatSnackBar } from '@angular/material'
 import { DataService } from '../services/data.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'my-collection',
@@ -17,6 +18,7 @@ export class CollectionComponent implements OnInit {
   showOperatingHours: boolean;
   openingTime: Date;
   closingTime: Date;
+  searchTerm$ = new Subject<string>();
 
   constructor(private _snackBar: MatSnackBar, private _dataService: DataService) {
     this.openingTime = new Date();
@@ -26,7 +28,8 @@ export class CollectionComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.books = this._dataService.getBooks();
+    //this.getBooks();
+    this._dataService.search(this.searchTerm$).subscribe(books => { this.books = books; });
   }
 
   updateMessage(message: string, type: string): void {
@@ -39,6 +42,12 @@ export class CollectionComponent implements OnInit {
 
   onRatingUpdate(book: Ibook): void {
     this.updateMessage(book.title, "Rating has been updated");
+  }
+
+  getBooks(): void {
+    this._dataService.getBooks().subscribe(
+      books => this.books = books,
+    error => this.updateMessage(<any>error, 'error'));
   }
 
 }
